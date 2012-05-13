f613c7785d59053d23053a6f9f749ae76d16192d
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2012-05-12 22:00:36 -0500

    will keep my modifications relevant to node-clojurescript on this branch, so as to better track and merge changes with the official repo

--- src/cljs/cljs/nodejs.cljs
+++ src/cljs/cljs/nodejs.cljs
@@ -3,9 +3,26 @@
 ; ClojureScript's 'print' set up correctly.
 (ns cljs.nodejs)
 
+; http://nodejs.org/api/globals.html
 ; Define namespaced references to Node's externed globals:
-(def require (js* "require"))
+(def global (js* "global"))
 (def process (js* "process"))
+(def console (js* "console"))
+(def buffer (js* "Buffer"))
+(def require (js* "require"))
+(def __filename (js* "__filename"))
+(def __dirname (js* "__dirname"))
+(def module (js* "module"))
+(def exports (js* "exports"))
+(def timeout (js* "setTimeout"))
+(def clear-timeout (js* "clearTimeout"))
+(def interval (js* "setInterval"))
+(def clear-interval (js* "clearInterval"))
+(def next-tick (.-nextTick process))
 
-; Have ClojureScript print using Node's sys.print function
-(set! cljs.core/string-print (.-print (require "sys")))
+; Have ClojureScript print using Node's process.stdout.write function
+(set! cljs.core/string-print (fn [s]
+  (.call (.-write (.-stdout process)) (.-stdout process) s)))
+
+; cljs.core/string-print does not append a newline. To print strings
+; with a newline automatically appended use cljs.core/println
--- src/cljs/cljs/nodejscli.cljs
+++ src/cljs/cljs/nodejscli.cljs
@@ -1,9 +1,4 @@
 ; Projects compiled with :target :nodejs have this file appended.  Its
-; job is to make sure cljs.nodejs is loaded and that the *main-cli-fn*
-; is called with the script's command-line arguments.
+; job is to make sure cljs.nodejs is loaded.
 (ns cljs.nodejscli
   (:require [cljs.nodejs :as nodejs]))
-
-; Call the user's main function
-(apply cljs.core/*main-cli-fn* (drop 2 (.-argv nodejs/process)))
-
